790be696a0cde00f905bac541b68cf81b36dc868
Pranav Dhingra <pranav.dhingra@gmail.com>
2018-11-05 18:50:57 -0600

    edited gitignore

--- LinkedInProfileUtil.py
+++ LinkedInProfileUtil.py
@@ -29,3 +29,25 @@ profiles = [{
 # profiles = [ item for item in results["items"] if "www.linkedin.com/in" in item["formattedUrl"]]
 
 print json.dumps(profiles, indent=2)
+# this works ^
+
+# This doesn't work, linkedin's gotten a lot smarter about preventing scraping.
+def get_linkedin_profile_details(profile_url):
+    headers = {
+        'User-Agent': "Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3",
+        # 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        # 'accept-encoding': 'gzip, deflate, sdch, br'
+    }
+    
+    res = req.get(profile_url, headers=headers)
+    data = res.text
+    print data
+    # print res.headers
+    # print dir(res)
+    # print res.status_code
+    soup = bs(data, features="html.parser")
+    print soup.find_all("div", id="summary")
+
+print profiles[0]["profile_url"]
+
+get_linkedin_profile_details(profiles[0]["profile_url"])
